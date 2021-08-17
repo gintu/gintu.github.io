b47325b633a881b7e00779c81fa8d41d0063add3
gintu <gintutom@gmail.com>
2021-08-18 01:28:34 +0530

    first push public

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  pathPrefix: "/gintu.github.io",
+  plugins: [],
+};
