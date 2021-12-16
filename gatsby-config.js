a200b0cd1ac4b453e0ce6a5697c2afafd1ec218f
gintu <gintutom@gmail.com>
2021-12-17 00:35:10 +0530

    remove font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,5 @@ module.exports = {
     siteUrl: `https://www.gintu.github.io`,
   },
   pathPrefix: "/",
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-minify`,
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: ["Rubik:400,500,700", "sans-serif"],
-        },
-      },
-    },
-  ],
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-minify`],
 };
