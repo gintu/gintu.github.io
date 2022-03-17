ed41ac833292094fbd0b8cc641cac7144e352cbb
gintu <gintutom@gmail.com>
2022-03-18 00:11:12 +0530

    update story

--- src/sections/story/story.js
+++ src/sections/story/story.js
@@ -9,16 +9,17 @@ export default function story() {
         <div>
           <p>
             I am an ordinary person who loves to code. I have been coding for
-            last 9 years and I am currently working at Envestnet as a senior
-            frontend engineer. I started off making C++ console games in school
-            and later on I started making websites. I have a keen eye for the
+            the past 9 years and I am currently working at Envestnet as a Senior
+            Frontend Engineer. I started off making C++ console games in school
+            and later on, I started making websites. I have a keen eye for
             design and was once ready to give up everything for cool, flashy
-            websites. I started by professional journey as a fullstack developer
-            and later converged to frontend as I enjoyed it more.
+            websites. I started my professional journey as a Full-stack
+            developer and later switched to Frontend as I enjoyed it more.
           </p>
           <p>
             I am a big fan of technology and recently got interested in
-            economics. I do little bit of travelling and photography.
+            economics. I do a little bit of traveling and photography. I used to
+            read a lot when I was younger, but now just web surfing and Netflix
           </p>
         </div>
       </Fade>
