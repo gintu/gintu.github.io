b4e094ee0f7e9fbb951658759be0c928e5030066
gintu <gintutom@gmail.com>
2026-02-25 02:40:21 +0530

    migration initial commit

--- src/components/underline/Underline.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Underline.module.css';
-
-interface UnderlineProps {
-    width: string;
-}
-
-export default function Underline({ width }: UnderlineProps) {
-    return <div className={styles.underline} style={{ width }} />;
-}
